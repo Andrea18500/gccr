d0dccde42c571fd5a00079562c64b399bebbdb17
Andrea18500 <andrea18500@hotmail.it>
2021-10-05 12:56:06 +0200

    Fixed error with cp finally

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 # Andrea Tosi   05/10/21    v0.1
 
 # Installer for gccr
-mkdir ~/bash_scripts
-cp ./gccr.sh ~/bash_scripts
+mkdir '~/bash_scripts'
+cp './gccr.sh' '~/bash_scripts'
 
 printf "\n\n# Alias for the shortcut gccr\nalias gccr='bash ~/bash_scripts/gccr.sh'" >> ~/.bashrc
