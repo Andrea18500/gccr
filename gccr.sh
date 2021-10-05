0f851f704e2c719ab5409fe6d68b7b8939cd8bc2
Andrea18500 <andrea18500@hotmail.it>
2021-10-05 12:24:07 +0200

    Added core files from local repository

---
+++ gccr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Andrea Tosi   05/10/21    v0.1
+
+# gccr : "gcc run". A little shortcut to not be overloaded with binary files
+# Compile file given with gcc, execute the file and deletes it.
+
+suff=".c"
+output=${1%$suff}
+gcc $1 -o $output && ./$output && rm $output
