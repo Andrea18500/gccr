ce114abe5761324b683aff74d131badc8af3d524
Anthor <andrea18500@hotmail.it>
2022-02-08 11:33:38 +0100

    Finally added support for up to 4 arguments that can be sent both to the program and to gcc. The arguments to the compiler have to start with "+" instead of "-". The first argument after gccr has to be the c file path. Es "path/to/filename.c".
    An example of a possible execution is:
    gccr myprograms/program.c +lm file_input.txt file_output.txt

--- gccr.sh
+++ gccr.sh
@@ -1,10 +1,107 @@
 #!/bin/bash
 
-# Andrea Tosi   05/10/21    v0.1
+# Andrea Tosi   05/10/21    v0.3
 
 # gccr : "gcc run". A little shortcut to not be overloaded with binary files
 # Compile file given with gcc, execute the file and deletes it.
 
 suff=".c"
+pref="-"
 output=${1%$suff}
-gcc $1 -o $output && ./$output $2 $3 $4 $5 && rm $output
+compiler_arg1=""
+program_arg1=""
+compiler_arg2=""
+program_arg2=""
+compiler_arg3=""
+program_arg3=""
+compiler_arg4=""
+program_arg4=""
+
+case "$#" in
+	2)
+	# arg1
+	if [[ $2 =~ \+lm ]]
+	then
+		compiler_arg1=${2/\+/$pref}
+	else
+		program_arg1=$2
+	fi
+	;;
+
+	3)
+	# arg1
+	if [[ $2 =~ \+* ]]
+	then
+		compiler_arg1=${2/\+/$pref}
+	else
+		program_arg1=$2
+	fi
+	# arg2
+	if [[ $3 =~ \+* ]]
+	then
+		compiler_arg2=${3/\+/$pref}
+	else
+		program_arg2=$3
+	fi
+	;;
+
+	4)
+	# arg1
+	if [[$2 =~ \+*]]
+	then
+		compiler_arg1=${2/\+/$pref}
+	else
+		program_arg1=$2
+	fi
+	# arg2
+	if [[$3 =~ \+*]]
+	then
+		compiler_arg2=${3/\+/$pref}
+	else
+		program_arg2=$3
+	fi
+	# arg3
+	if [[$4 =~ \+*]]
+	then
+		compiler_arg3=${4/\+/$pref}
+	else
+		program_arg3=$4
+	fi
+	;;
+
+	4)
+	# arg1
+	if [[$2 =~ \+*]]
+	then
+		compiler_arg1=${2/\+/$pref}
+	else
+		program_arg1=$2
+	fi
+	# arg2
+	if [[$3 =~ \+*]]
+	then
+		compiler_arg2=${3/\+/$pref}
+	else
+		program_arg2=$3
+	fi
+	# arg3
+	if [[$4 =~ \+*]]
+	then
+		compiler_arg3=${4/\+/$pref}
+	else
+		program_arg3=$4
+	fi
+	# arg4
+	if [[$5 =~ \+*]]
+	then
+		compiler_arg4=${5/\+/$pref}
+	else
+		program_arg4=$5
+	fi
+	;;
+esac
+
+# commands
+gcc $1 -o $output $compiler_arg1 $compiler_arg2 $compiler_arg3 $compiler_arg4 &&
+./$output $program_arg1 $program_arg2 $program_arg3 $program_arg4 &&
+rm $output
